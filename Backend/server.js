6144db3e68f256ec40e20733093c326dccae4741
SaaSProvider-1 <Subhamsahu51006@gmail.com>
2025-09-30 18:53:42 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -57,7 +57,6 @@ const corsOptions = {
     process.env.FRONTEND_URL || 'http://localhost:5173',
     'http://localhost:3000',
     'http://localhost:5173',
-    'http://localhost:5174',
     "https://cafe-website-yv8t.onrender.com"
   ],
   credentials: true,
